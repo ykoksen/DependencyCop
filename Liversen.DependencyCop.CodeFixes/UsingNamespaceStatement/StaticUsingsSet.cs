171919ecf96835a1f1d7c6a1ffb86312c7f901f5
Morten Hartlev Lindhart <MORTL@danskebank.dk>
2026-03-09 07:47:57 +0100

    - Added tests for fixer. - Added fixer. - Refactored a bit.

---
+++ Liversen.DependencyCop.CodeFixes/UsingNamespaceStatement/StaticUsingsSet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Liversen.DependencyCop.UsingNamespaceStatement
+{
+    class StaticUsingsSet
+    {
+        readonly List<SyntaxNode> innerList = new List<SyntaxNode>();
+
+        public static async Task<StaticUsingsSet> GetExisingStaticUsings(Document document, CancellationToken cancellationToken)
+        {
+            var nodeSet = new StaticUsingsSet();
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            if (root == null)
+            {
+                return nodeSet;
+            }
+
+            var usingDirectives = root.DescendantNodes().OfType<UsingDirectiveSyntax>().Where(x => !string.IsNullOrEmpty(x.StaticKeyword.Text));
+
+            foreach (var directive in usingDirectives)
+            {
+                nodeSet.Add(directive);
+            }
+
+            return nodeSet;
+        }
+
+        public bool Add(SyntaxNode usingDirective)
+        {
+            if (innerList.Exists(x => x.IsEquivalentTo(usingDirective, true)))
+            {
+                return false;
+            }
+
+            innerList.Add(usingDirective);
+            return true;
+        }
+    }
+}
---
+++ Liversen.DependencyCop.Tests/UsingNamespaceStatement/TestData/Fixer/GenericViolationWithNormalViolationTestFixedCode.cs
@@ -0,0 +1,23 @@
+﻿
+namespace UsingNamespaceStatementAnalyzer.Account
+{
+    class Id<T>
+    {
+        public T Value { get; set; }
+    }
+
+    class Item
+    {
+        public string Name { get; set; }
+    }
+}
+
+namespace UsingNamespaceStatementAnalyzer.Transaction
+{
+    class Current
+    {
+        public Account.Id<Account.Id<Account.Id<Account.Id<Account.Item>>>> AccountId { get; set; }
+
+        public string Text { get; set; }
+    }
+}
